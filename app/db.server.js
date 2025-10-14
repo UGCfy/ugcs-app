7beb9d048c39857756a7638ddfc109a03293ce7f
Farooq Ali Khan <farooqalikhan@Farooqs-MacBook-Air.local>
2025-10-14 21:23:52 +0500

    fix: Prisma ESM/CJS import for Render

--- app/db.server.js
+++ app/db.server.js
@@ -1,11 +1,5 @@
-import { PrismaClient } from "@prisma/client";
+// app/db.server.js
+import pkg from '@prisma/client';
+const { PrismaClient } = pkg;
 
-if (process.env.NODE_ENV !== "production") {
-  if (!global.prismaGlobal) {
-    global.prismaGlobal = new PrismaClient();
-  }
-}
-
-const prisma = global.prismaGlobal ?? new PrismaClient();
-
-export default prisma;
+export const prisma = new PrismaClient();
